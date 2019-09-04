4d75669279479cbaf19eb4fe4850fcd445bc49cc
Merlin <merlinzhao@gmail.com>
2019-09-03 22:54:27 -0400

    animations on intro

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <div class="intro">
-      <div class="innerIntro">
-        <p class="MERLIN">MERLIN.</p>
-      </div>
+      <p class="MERLIN">MERLIN.</p>
+      <p>hello</p>
     </div>
 
     <landing />
@@ -28,141 +27,108 @@ export default {
   },
   mounted() {
     const intro = document.querySelector(".intro");
-    setTimeout();
   }
 };
 </script>
 
 <style scoped>
 .intro {
+  font-size: 1200%;
   position: absolute;
   height: 100%;
   width: 100%;
   background: #f4f4f4;
 
-  -webkit-animation: bgfadein 1.5s ease-in; /* Safari, Chrome and Opera > 12.1 */
-  -moz-animation: bgfadein 1.5s ease-in; /* Firefox < 16 */
-  -ms-animation: bgfadein 1.5s ease-in; /* Internet Explorer */
-  -o-animation: bgfadein 1.5s ease-in; /* Opera < 12.1 */
-  animation: bgfadein 1.5s ease-in;
-  z-index: 1;
-}
-
-@keyframes bgfadein {
-  from {
-    background: #131313;
-  }
-  to {
-    background: #f4f4f4;
-  }
-}
-
-/* Firefox < 16 */
-@-moz-keyframes bgfadein {
-  from {
-    background: #131313;
-  }
-  to {
-    background: #f4f4f4;
-  }
-}
-
-/* Safari, Chrome and Opera > 12.1 */
-@-webkit-keyframes bgfadein {
-  from {
-    background: #131313;
-  }
-  to {
-    background: #f4f4f4;
-  }
-}
-
-/* Internet Explorer */
-@-ms-keyframes bgfadein {
-  from {
-    background: #131313;
-  }
-  to {
-    background: #f4f4f4;
-  }
-}
-
-/* Opera < 12.1 */
-@-o-keyframes bgfadein {
-  from {
-    background: #131313;
-  }
-  to {
-    background: #f4f4f4;
-  }
-}
-
-/* ==========================INNER INTRO INNER INTRO INNER INTRO INNRE INTRO ====================== */
-/* ==========================INNER INTRO INNER INTRO INNER INTRO INNER INTRO ======================= */
-/* =============================================================================================== */
-
-.innerIntro {
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  background: linear-gradient(314deg, #ffa600, #c401ff, #0aefff);
-  background-size: 600% 600%;
   display: flex;
   flex-direction: column;
   -webkit-box-orient: vertical;
   justify-content: center;
-  align-items: left;
-  text-align: center;
+  align-items: center;
 
-  background-clip: text;
-  -webkit-background-clip: text;
+  -webkit-animation: introFade 4.5s ease-out;
+  -moz-animation: introFade 4.5s ease-out;
+  animation: introFade 4.5s ease-out;
 
-  -webkit-animation: introGradientTwo 4s linear infinite;
-  -moz-animation: introGradientTwo 4s linear infinite;
-  animation: introGradientTwo 4s linear infinite;
+  visibility: hidden;
+
+  z-index: 1;
+}
+
+@-webkit-keyframes introFade {
+  0% {
+    visibility: visible;
+    background: #131313;
+  }
+  15% {
+    background: #131313;
+  }
+  50% {
+    background: #131313;
+  }
+  70% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+    visibility: hidden;
+    background: #f4f4f4;
+  }
+}
+@-moz-keyframes introFade {
+  0% {
+    visibility: visible;
+    background: #131313;
+  }
+  15% {
+    background: #131313;
+  }
+  50% {
+    background: #131313;
+  }
+  70% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+    visibility: hidden;
+    background: #f4f4f4;
+  }
+}
+@keyframes introFade {
+  0% {
+    visibility: visible;
+    background: #131313;
+  }
+  15% {
+    background: #131313;
+  }
+  50% {
+    background: #131313;
+  }
+  70% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+    visibility: hidden;
+    background: #f4f4f4;
+  }
 }
 
 /* =============================================================================================== */
 /* =============================================================================================== */
 /* =============================================================================================== */
-
-@-webkit-keyframes GradientAnimation {
-  0% {
-    background-position: 0% 48%;
-  }
-  50% {
-    background-position: 100% 53%;
-  }
-  100% {
-    background-position: 0% 48%;
-  }
-}
-@-moz-keyframes GradientAnimation {
-  0% {
-    background-position: 0% 48%;
-  }
-  50% {
-    background-position: 100% 53%;
-  }
-  100% {
-    background-position: 0% 48%;
-  }
-}
-@keyframes GradientAnimation {
-  0% {
-    background-position: 0% 48%;
-  }
-  50% {
-    background-position: 100% 53%;
-  }
-  100% {
-    background-position: 0% 48%;
-  }
-}
 /* =============================================================================================== */
 @-webkit-keyframes introGradientTwo {
   0% {
+    /* font-size: 3em; */
     background-position: 7% 0%;
+  }
+  35% {
+    font-size: 1em;
   }
   50% {
     background-position: 94% 100%;
@@ -173,7 +139,11 @@ export default {
 }
 @-moz-keyframes introGradientTwo {
   0% {
+    /* font-size: 3em; */
     background-position: 7% 0%;
+  }
+  35% {
+    font-size: 1em;
   }
   50% {
     background-position: 94% 100%;
@@ -184,7 +154,11 @@ export default {
 }
 @keyframes introGradientTwo {
   0% {
+    /* font-size: 5em; */
     background-position: 7% 0%;
+  }
+  35% {
+    font-size: 1em;
   }
   50% {
     background-position: 94% 100%;
@@ -199,65 +173,25 @@ export default {
 /* =============================================================================================== */
 
 .MERLIN {
-  font-size: 1200%;
   font-weight: 700;
   text-align: center;
 
-  -webkit-animation: fadein 3s; /* Safari, Chrome and Opera > 12.1 */
-  -moz-animation: fadein 3s; /* Firefox < 16 */
-  -ms-animation: fadein 3s; /* Internet Explorer */
-  -o-animation: fadein 3s; /* Opera < 12.1 */
-  animation: fadein 3s;
+  position: absolute;
+  background: linear-gradient(314deg, #ffa600, #c401ff, #0aefff);
+  background-size: 600% 600%;
+  display: block;
+  flex-direction: column;
+  -webkit-box-orient: vertical;
+  justify-content: center;
 
-  color: #f4f4f400;
-}
+  background-clip: text;
+  -webkit-background-clip: text;
 
-@keyframes fadein {
-  from {
-    color: #131313;
-  }
-  to {
-    color: #f4f4f400;
-  }
-}
+  color: transparent;
+  -webkit-text-fill-color: rgba(255, 0, 0, 0);
 
-/* Firefox < 16 */
-@-moz-keyframes fadein {
-  from {
-    color: #131313;
-  }
-  to {
-    color: #f4f4f400;
-  }
-}
-
-/* Safari, Chrome and Opera > 12.1 */
-@-webkit-keyframes fadein {
-  from {
-    color: #131313;
-  }
-  to {
-    color: #f4f4f400;
-  }
-}
-
-/* Internet Explorer */
-@-ms-keyframes fadein {
-  from {
-    color: #131313;
-  }
-  to {
-    color: #f4f4f400;
-  }
-}
-
-/* Opera < 12.1 */
-@-o-keyframes fadein {
-  from {
-    color: #131313;
-  }
-  to {
-    color: #f4f4f400;
-  }
+  -webkit-animation: introGradientTwo 3s ease-out;
+  -moz-animation: introGradientTwo 3s ease-out;
+  animation: introGradientTwo 3s ease-out;
 }
 </style>
